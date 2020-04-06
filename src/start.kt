adbca52b8fa89d7ffdfe96e3480b68b71327ec70
Fabiana Assami <bianavic@gmail.com>
2020-04-06 09:49:24 -0300

    criando interface da agenda

--- src/start.kt
+++ src/start.kt
@@ -1,3 +1,5 @@
+import ui.Mainform
+
 fun main() {
     Mainform()
 }
--- src/ui/Mainform.java
+++ src/ui/Mainform.java
@@ -2,6 +2,20 @@ package ui;
 
 import javax.swing.*;
 
+// classe agrupa atributos e funcoes (acoes)
 public class Mainform extends JFrame {
-    private JPanel panell;
+    // ATRIBUTOS
+    private JPanel rootPanel;
+    private JButton buttonNewContact;
+    private JButton buttonRemove;
+    private JTable tableContacts;
+
+    //CONSTRUTOR = é algo que é executado qdo a classe é criada
+    public Mainform() {
+
+        // chamar funcoes para mostrar na tela (comportamentos do Java Swing)
+        setContentPane(rootPanel);
+        setSize(500, 250);
+        setVisible(true);
+    }
 }
