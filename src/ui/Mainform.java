f7f95866c3e8d437298e9d6fa5f50f53dc2ba64a
Fabiana Assami <bianavic@gmail.com>
2020-04-06 10:35:21 -0300

    criando interface do formulario da  agenda

--- src/ui/Mainform.java
+++ src/ui/Mainform.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 // classe agrupa atributos e funcoes (acoes)
 public class Mainform extends JFrame {
@@ -17,5 +18,11 @@ public class Mainform extends JFrame {
         setContentPane(rootPanel);
         setSize(500, 250);
         setVisible(true);
+
+        // posicao/local de abertura da janela
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
