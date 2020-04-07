c7569402494e7520e2ea5b35c0b9328ec5bfa095
Fabiana Assami <bianavic@gmail.com>
2020-04-07 15:18:20 -0300

    centralizada a agenda na tela

--- src/ui/Mainform.java
+++ src/ui/Mainform.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // classe agrupa atributos e funcoes (acoes)
 public class Mainform extends JFrame {
@@ -24,5 +26,26 @@ public class Mainform extends JFrame {
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // faz a atribuicao dos eventos
+        setListeners();
+    }
+
+    // criar funcao que ouve
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                new ContactForm(); // estou invocando um formulario novo
+                dispose();
+            }
+        });
+
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
     }
 }
