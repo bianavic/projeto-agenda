87c847da64cc6a1997180df019b67f1cde510ef1
Fabiana Assami <bianavic@gmail.com>
2020-04-08 15:22:47 -0300

    implementada logica para deletar contato

--- src/repository/ContactRepository.kt
+++ src/repository/ContactRepository.kt
@@ -20,6 +20,17 @@ class ContactRepository {
         }
 
         fun delete(contact: ContactEntity) {
+            //criar logica responsavel por deletar o contato da listagem
+
+            // contactList.remove(contact)
+            var index = 0
+            for (item in contactList.withIndex()) {
+                if (item.value.name == contact.name && item.value.phone == contact.phone) {
+                    index = item.index
+                    break
+                }
+            }
+            contactList.removeAt(index)
         }
 
         // criar um metodo (este metodo sera uma funcao) que seja capaz de
